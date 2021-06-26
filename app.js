84d85c600e03e90e732b278b518359c82b0dcaaf
Shahin <shahin@dev.local>
2021-06-26 17:18:42 +0600

    Update

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colorList = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//Btn
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+
+btn.addEventListener('click', function(){
+    // get random color number between 0 - 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    
+    document.body.style.backgroundColor = colorList[randomNumber];
+    color.textContent = colorList[randomNumber];
+});
+
+
+// Get random number
+function getRandomNumber() {
+    return Math.floor(Math.random() * colorList.length);
+}
